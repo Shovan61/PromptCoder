35f58343986671c1b799b41a32ab8a0a2cef00f8
Shovan61 <shovonmazumder61@gmail.com>
2026-05-31 01:13:09 +0200

    feat: implement Agent and AgentEvent classes for event handling

---
+++ agent/agent.py
@@ -0,0 +1,12 @@
+from typing import AsyncGenerator
+
+from agent.events import AgentEvent
+
+
+class Agent:
+    def __init__(self):
+        pass
+
+
+async def _agentic_loop(self) -> AsyncGenerator[AgentEvent]:
+    pass
--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import asyncio
+from functools import wraps
 from client.llm_client import LLMClient
 
 
-async def main():
+import click
+
+
+class CLI:
+    def __init__(self):
+        pass
+
+    def run_single():
+        pass
+
+
+def async_command(f):
+    """Decorator to make Click work with async functions"""
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+
+    return wrapper
+
+
+@click.command()
+@click.argument("prompt", required=False)
+@async_command
+async def main(prompt: str | None):
+    if prompt is None:
+        # If no prompt provided, show help
+        click.echo("Please provide a prompt. Usage: python main.py 'Your prompt here'")
+        return
+
+    print(prompt)
     client = LLMClient()
-    messages = [{"role": "user", "content": "Whats up?"}]
-    async for event in client.chat_completetion(messages, False):
+    messages = [{"role": "user", "content": prompt}]
+    async for event in client.chat_completetion(messages, True):
         print(event)
     await client.close()
- 
 
-asyncio.run(main())
+
+main()
